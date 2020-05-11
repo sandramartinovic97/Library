8221a0dd4ba70565321aed2140f21c39840ff34d
jelenapetrovacki <jpetrovacki3@gmail.com>
2020-05-11 18:35:27 +0200

    MySQL connection

--- Library/backend/library/src/main/java/com/library/library/LibraryApplication.java
+++ Library/backend/library/src/main/java/com/library/library/LibraryApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
+
+		String url = 'jdbc:mysql://localhost:3306/?user=root';
+		String user = 'root';
+		String password = '0637263646'
+		Connection myConnection = DriveManager.getConnection(url,user,password);
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
