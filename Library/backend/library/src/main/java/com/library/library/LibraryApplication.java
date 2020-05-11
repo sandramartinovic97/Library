e34afa674b7bb51d17b718a76b2f57956e80a30a
DejanaGladic <dejana023@gmail.com>
2020-05-11 18:57:39 +0200

    Updated connection

--- Library/backend/library/src/main/java/com/library/library/LibraryApplication.java
+++ Library/backend/library/src/main/java/com/library/library/LibraryApplication.java
@@ -3,15 +3,21 @@ package com.library.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 
-		String url = 'jdbc:mysql://localhost:3306/?user=root';
-		String user = 'root';
-		String password = '0637263646'
-		Connection myConnection = DriveManager.getConnection(url,user,password);
+		String url = "jdbc:mysql://localhost:3306/?user=root";
+		String user = "root";
+		String password = "0637263646";
+		try {
+			Connection myConnection = DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
