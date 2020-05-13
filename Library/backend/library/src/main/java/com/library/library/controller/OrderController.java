40e98f1b8c7a799f14b9da0ce416ff717121c1ba
saragajic115 <saragajic115@gmail.com>
2020-05-13 15:59:12 +0200

    Added order controller

---
+++ Library/backend/library/src/main/java/com/library/library/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.library.library.controller;
+
+import com.library.library.model.Order;
+import com.library.library.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping("orders")
+    public Collection<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
