9d13f08dc75e8150d6be30da72541ed1c6987ad2
DejanaGladic <dejana023@gmail.com>
2020-05-13 17:30:00 +0200

    Book model and data insert created

--- Library/backend/library/src/main/java/com/library/library/model/Book.java
+++ Library/backend/library/src/main/java/com/library/library/model/Book.java
@@ -1,7 +1,33 @@
-/*
+
 package com.library.library.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String bookName;
+    @Column
+    private String bookAuthor;
+    @Column
+    private String bookDescription;
+    @Column
+    private String bookPublisher;
+    @Column
+    private String bookLanguage;
+    @Column
+    private Integer bookYear;
+    @Column
+    private Double bookPrice;
+    @Column
+    private Integer bookQuantity;
+
 }
-*/
+
