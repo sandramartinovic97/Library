1ab1d30e9423ba3915da6345c9ba32ad6db2bb48
saragajic115 <saragajic115@gmail.com>
2020-05-13 17:43:37 +0200

    Added method for get all book orders

---
+++ Library/backend/library/src/main/java/com/library/library/repository/BookOrderRepository.java
@@ -0,0 +1,11 @@
+
+package com.library.library.repository;
+
+import com.library.library.model.BookOrder;
+import com.library.library.model.BookOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookOrderRepository extends JpaRepository<BookOrder,Integer> {
+
+}
+
--- Library/backend/library/src/main/resources/data.sql
+++ Library/backend/library/src/main/resources/data.sql
@@ -3,3 +3,5 @@ insert into Genre (genre_name) values ('Thriller');
 insert into Genre (genre_name) values ('Mystery');
 insert into Genre (genre_name) values ('Novel');
 insert into Genre (genre_name) values ('Fantasy');
+
+insert into Book_Order (order_date,order_price,order_status) values (parsedatetime('17-09-2012 18:47:52.690', 'dd-MM-yyyy hh:mm:ss.SS'), 1000,'Complete');
