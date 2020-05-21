2d08bb1cb47052e85e4a4813eff27b84921b3559
sandramartinovic97 <sandramartinovic.sm@gmail.com>
2020-05-21 22:11:17 +0200

    Added new inserts in data.sql

--- Library/backend/library/src/main/resources/data.sql
+++ Library/backend/library/src/main/resources/data.sql
@@ -1,35 +1,139 @@
-insert into Genre (genre_name) values ('Love');
-insert into Genre (genre_name) values ('Thriller');
-insert into Genre (genre_name) values ('Mystery');
+insert into Admin (admin_username, admin_email, admin_password) values ('admin', 'admin@gmail.com', 'admin123');
+
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Marko','Marković','m','+38162564736','mmarkovic@gmail.com','Serbia','Novi Sad','Bulevar Oslobođenja bb','marko');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Petar','Petrović','m','+38163444736','ppetrovic@gmail.com','Serbia','Novi Sad','Bulevar Oslobođenja bb','petar');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Milica','Mitrović','z','+38164258146','mmitrovic@gmail.com','Serbia','Beograd','Knez Mihajlova 23','milica');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Nataša','Petrović','z','+381668963254','natasa.pet@gmail.com','Serbia','Novi Sad','Jevrejska 5','natasa');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Milan','Nikolić','m','+381614526895','nikolicmilan@gmail.com','Serbia','Subotica','Banijska 17','milan');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Sanja','Matić','z','+381635795168','matics@gmail.com','Serbia','Beograd','Balkanska 11','sanja');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Zorana','Ilić','z','+38164126894','zoranailic@gmail.com','Serbia','Sombor','Rade Končara 17','zorana');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Ana','Anić','z','+381605896496','ana.anic@gmail.com','Serbia','Beograd','Pavla Pavlovića 112','ana');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Milena','Matić','z','+381628463169','maticmilena@gmail.com','Serbia','Novi Sad','Bulevar Evrope 89','milena');
+insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
+                        customer_email,customer_country,customer_city,customer_street,customer_password)
+values ('Nikola','Ilić','m','+38163579961','nikolailic5@gmail.com','Serbia','Kraljevo','Dušana Popovića 7','nikola');
+
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('The Great Gatsby', 'F. Scott Fitzgerald',
+        'The story of this novel primarily concerns the young and mysterious millionaire Jay Gatsby and his passion and obsession with the beautiful former debutante Daisy Buchanan.',
+        'Vulkan', 'Srpski', 1925, 500.00, 10);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Harry Potter and the Philosopher’s Stone', 'J. K. Rowling',
+        'A young wizard discovers his magical heritage when he receives a letter of acceptance to Hogwarts School. Harry with the help of his friends faces the dark wizard Lord Voldemort.',
+        'Vulkan', 'Srpski', 1997, 600.00, 5);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('A Brief History of Time: From the Big Bang to Black Holes', 'Stephen Hawking',
+        'This book attempts to explain a range of subjects in cosmology, including the Big Bang, black holes etc. His main goal is to give an overview of the subject, but he also attempts to explain some complex mathematics',
+        'Laguna', 'Engleski', 1988, 900.00, 2);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Jamie’s 15-Minute Meals', 'Jamie Oliver',
+        'This is a classic cookbook meant to arm readers with the skills to create great meals quickly. These are some of the quickest and easiest meals Jamie’s ever done.',
+        'Delfi', 'Srpski', 2012, 1050.00, 5);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('The notebook', 'Nicholas Sparks',
+        'A contemporary love story set in the pre- and post-World War II era. Noah and Allie spend a wonderful summer together, but her family and the socio-economic realities of the time prevent them from being together.',
+        'Laguna', 'Srpski', 1996, 670.00, 8);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Fantastic Beasts and Where to Find Them', 'J. K. Rowling',
+        'This is a guide book written about the magical creatures of the wizarding world. Some of the beasts will be familiar to readers of the Harry Potter books and others will be a surprise.',
+        'Laguna', 'Srpski', 2001, 750.00, 4);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Batman: Knightfall', 'Doug Moench',
+        'The Knightfall Protocol was the last of Batman’s backup plans, to be deployed only when his secret identity was compromised in order to protect those closest to him. It was unknown when Batman devised that final contingency plan.',
+        'Laguna', 'Srpski', 1993, 500.00, 12);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('The Fault in Our Stars', 'John Green',
+        'A 16 year old cancer patient meets and falls in love with a guy, a similarly afflicted teen from her cancer support group. Their story talks about love, sadness and romance. In meanwhile they are searching for the author of their favourite book.',
+        'Delfi', 'Engleski', 2012, 690.00, 6);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Becoming', 'Michelle Obama',
+        'Memoir of former United States first lady Michelle Obama. The book talks about author’s roots and how she found her voice, as well as her time in the White House, her public health campaign, and her role as a mother.',
+        'Laguna', 'Engleski', 2018, 810.00, 4);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Gordon Ramsay Quick and Delicious: 100 Recipes to Cook in 30 Minutes or Less', 'Gordon Ramsay',
+        'The result of Gordon’s career is described within 100 tried and tested recipes that you’ll find yourself using time and again. All the recipes take 30 minutes or less.',
+        'Laguna', 'Srpski', 2019, 1400.00, 5);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Amazing Spider-Man #149', 'Stan Lee',
+        'Peter discovers that his biology professor, Dr. Warren, has a few loose screws. As Jackal, Warren reveals his love for Gwen and the secret behind her resurrection. Will Spider-Man be able to talk sense into his professor before Jackal takes Ned’s life?',
+        'Laguna', 'Srpski', 1975, 540.00, 8);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('A walk to remember', 'Nicholas Sparks',
+        'This story is set in 1958–1959 in Beaufort, North Carolina, and it is about two teenagers who fall in love with each other despite the disparity of their personalities. Although the story is largely fictional, certain parts were based on real experiences.',
+        'Vulkan', 'Srpski', 1999, 470.00, 15);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Dear John', 'Nicholas Sparks',
+        'When soldier meets an idealistic college student, it’s the beginning of a strong romance. Over the next seven years and separated by John’s dangerous deployment, the lovers stay in touch through their letters, meeting in person only rarely.',
+        'Vulkan', 'Srpski', 2007, 390.00, 7);
+insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
+values ('Winter dreams', 'F. Scott Fitzgerald',
+        'The story was based upon Fitzgerald’s unsuccessful romantic pursuit of socialite Ginevra. While teenagers, they met at a party and shared an romance (1915-1917), but their relationship soon ended when Ginevra’s father publicly humiliated the young writer.',
+        'Delfi', 'Engleski', 1922, 490.00, 2);
+
+insert into Genre (genre_name) values ('Romance');
 insert into Genre (genre_name) values ('Novel');
+insert into Genre (genre_name) values ('Mystery');
+insert into Genre (genre_name) values ('Comics');
 insert into Genre (genre_name) values ('Fantasy');
+insert into Genre (genre_name) values ('Biography');
+insert into Genre (genre_name) values ('Short story');
+insert into Genre (genre_name) values ('Cookbook');
+insert into Genre (genre_name) values ('Science');
+insert into Genre (genre_name) values ('Thriller');
 
-insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
-                        customer_email,customer_country,customer_city,customer_street,customer_password)
-values ('Marko','Markovic','m','+38162564736','mmarkovic@gmail.com','Serbia','Novi Sad','Bulevar Oslobodjenja bb','marko');
-insert into Customer (customer_name,customer_surname,customer_gender,customer_phone_num,
-                        customer_email,customer_country,customer_city,customer_street,customer_password)
-values ('Petar','Petrovic','m','+38163444736','ppetrovic@gmail.com','Serbia','Novi Sad','Bulevar Oslobodjenja bb','petar');
-
-insert into Book_Order (order_date,order_price,order_status, customer_id) values (parsedatetime('17-09-2012 18:47:52.690', 'dd-MM-yyyy hh:mm:ss.SS'), 1000,'Complete',1);
-insert into Book_Order (order_date,order_price,order_status,customer_id) values (parsedatetime('17-09-2012 18:47:52.690', 'dd-MM-yyyy hh:mm:ss.SS'), 1,'Test',1);
-
-insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
-values ('The Great Gatsby', 'F. Scott Fitzgerald', 'The story of this novel primarily concerns the young and mysterious millionaire
-Jay Gatsby and his passion and obsession with the beautiful former debutante Daisy Buchanan.', 'Vulkan', 'Srpski', 1925, 500.00, 10);
-
-insert into Book (book_name, book_author, book_description, book_publisher, book_language, book_year, book_price, book_quantity)
-values ('Harry Potter and the Philosophers Stone', 'J. K. Rowling', 'A young wizard discovers his
-magical heritage when he receives a letter of acceptance to Hogwarts School.
-Harry with the help of his friends faces the dark wizard Lord Voldemort.', 'Vulkan', 'Srpski', 1997, 600.00, 5);
-
+insert into Book_Genre (book_id,genre_id) values (1,2);
+insert into Book_Genre (book_id,genre_id) values (2,5);
+insert into Book_Genre (book_id,genre_id) values (3,9);
+insert into Book_Genre (book_id,genre_id) values (4,8);
+insert into Book_Genre (book_id,genre_id) values (5,1);
+insert into Book_Genre (book_id,genre_id) values (6,5);
+insert into Book_Genre (book_id,genre_id) values (7,4);
+insert into Book_Genre (book_id,genre_id) values (8,1);
+insert into Book_Genre (book_id,genre_id) values (9,6);
+insert into Book_Genre (book_id,genre_id) values (10,8);
+insert into Book_Genre (book_id,genre_id) values (11,4);
+insert into Book_Genre (book_id,genre_id) values (12,1);
+insert into Book_Genre (book_id,genre_id) values (13,1);
+insert into Book_Genre (book_id,genre_id) values (14,7);
 
 insert into Favourite_Book (book_id,customer_id) values (1,1);
 insert into Favourite_Book (book_id,customer_id) values (2,1);
+insert into Favourite_Book (book_id,customer_id) values (3,10);
+insert into Favourite_Book (book_id,customer_id) values (4,3);
+insert into Favourite_Book (book_id,customer_id) values (5,6);
+insert into Favourite_Book (book_id,customer_id) values (5,7);
+insert into Favourite_Book (book_id,customer_id) values (6,3);
+insert into Favourite_Book (book_id,customer_id) values (7,5);
+insert into Favourite_Book (book_id,customer_id) values (8,7);
+insert into Favourite_Book (book_id,customer_id) values (9,9);
+insert into Favourite_Book (book_id,customer_id) values (13,7);
 
-insert into Book_Genre (book_id,genre_id) values (1,3);
-insert into Book_Genre (book_id,genre_id) values (2,5);
+insert into Book_Order (order_date,order_price,order_status,customer_id) values (parsedatetime('17-09-2012 18:47:52.690', 'dd-MM-yyyy hh:mm:ss.SS'), 500,'Delivered',1);
+insert into Book_Order (order_date,order_price,order_status,customer_id) values (parsedatetime('05-11-2014 20:14:12.500', 'dd-MM-yyyy hh:mm:ss.SS'), 300,'Cancelled',2);
+insert into Book_Order (order_date,order_price,order_status,customer_id) values (parsedatetime('22-10-2017 14:01:14.600', 'dd-MM-yyyy hh:mm:ss.SS'), 200,'On hold',3);
+insert into Book_Order (order_date,order_price,order_status,customer_id) values (parsedatetime('05-02-2018 08:11:24.400', 'dd-MM-yyyy hh:mm:ss.SS'), 320,'Delivered',3);
+insert into Book_Order (order_date,order_price,order_status,customer_id) values (parsedatetime('31-01-2020 11:27:55.620', 'dd-MM-yyyy hh:mm:ss.SS'), 450,'Delivered',5);
 
-insert into Admin (admin_username, admin_email, admin_password) values ('admin', 'admin@gmail.com', 'admin123');
-
-insert into Order_Item (order_id, book_id, item_price, item_quantity) values (1,1,1000.0,2);
+insert into Order_Item (order_id, book_id, item_price, item_quantity) values (1,1,1000.00,2);
+insert into Order_Item (order_id, book_id, item_price, item_quantity) values (2,2,900.00,1);
+insert into Order_Item (order_id, book_id, item_price, item_quantity) values (3,1,700.00,1);
+insert into Order_Item (order_id, book_id, item_price, item_quantity) values (4,11,840.00,1);
+insert into Order_Item (order_id, book_id, item_price, item_quantity) values (5,6,1000.00,2);
